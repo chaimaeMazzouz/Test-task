cb7968d62dbdce655d703df4fc8f8a36779a41d5
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2024-06-11 15:05:01 +0100

    Candidate Entity added

---
+++ Test-task/Entities/Candidate.cs
@@ -0,0 +1,15 @@
+﻿namespace Test_task.Entities
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PreferredCallTimeInterval { get; set; } 
+        public string LinkedInProfileUrl { get; set; }
+        public string GitHubProfileUrl { get; set; }
+        public string Comment { get; set; }
+    }
+}
